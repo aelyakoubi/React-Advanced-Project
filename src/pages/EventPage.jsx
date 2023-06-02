e383d7e0b0298c4736395c2366a03c7c38aef5e3
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2023-06-02 08:40:49 +0200

    Add files via upload

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Box, Heading, Button, Input, Flex } from "@chakra-ui/react";
+import { Box, Heading, Input, Flex, Button } from "@chakra-ui/react";
 import { useParams, useNavigate } from "react-router-dom";
+import { DeleteButton } from "../components/DeleteButton";
 
 export const EventPage = () => {
   const { eventId } = useParams();
@@ -58,6 +59,10 @@ export const EventPage = () => {
         console.log(error);
         alert("Failed to delete event!");
       });
+  };
+
+  const onDeleteConfirm = () => {
+    handleDeleteEvent();
   };
 
   if (!event) {
@@ -128,47 +133,30 @@ export const EventPage = () => {
             />
           </label>
         </form>
-        <Box mt={4}>
-          <Button
-            onClick={handleUpdateEvent}
-            type="submit"
-            borderRadius="15px"
-            border="3px solid"
-            padding="0.6em 1.2em"
-            fontSize="1em"
-            fontWeight="500"
-            fontFamily="inherit"
-            backgroundColor="grey.500"
-            cursor="pointer"
-            transition="border-color 0.25s, box-shadow 0.25s"
-            _hover={{
-              borderColor: "purple",
-              boxShadow: "0 0 8px 2px rgba(128, 78, 254, 0.5)",
-            }}
-            _focus={{ outline: "4px auto -webkit-focus-ring-color" }}
-          >
+        s
+        <Box
+          mt={4}
+          borderRadius="15px"
+          border="3px solid"
+          padding="0.6em 1.2em"
+          fontSize="1em"
+          fontWeight="extrabold"
+          fontFamily="inherit"
+          fontStyle="bold"
+          color={"black"}
+          cursor="pointer"
+          transition="border-color 0.25s, box-shadow 0.25s"
+          _hover={{
+            borderColor: "purple",
+            boxShadow: "0 0 8px 2px rgba(128, 78, 254, 0.5)",
+          }}
+          _focus={{ outline: "4px auto -webkit-focus-ring-color" }}
+        >
+          <Button onClick={handleUpdateEvent} type="submit" className="button">
             Edit Event
           </Button>
 
-          <Button
-            onClick={handleDeleteEvent}
-            borderRadius="15px"
-            border="3px solid"
-            padding="0.6em 1.2em"
-            fontSize="1em"
-            fontWeight="500"
-            fontFamily="inherit"
-            backgroundColor="grey.500"
-            cursor="pointer"
-            transition="border-color 0.25s, box-shadow 0.25s"
-            _hover={{
-              borderColor: "purple",
-              boxShadow: "0 0 8px 2px rgba(128, 78, 254, 0.5)",
-            }}
-            _focus={{ outline: "4px auto -webkit-focus-ring-color" }}
-          >
-            Delete Event
-          </Button>
+          <DeleteButton onDelete={onDeleteConfirm} />
         </Box>
       </Box>
     </Flex>
