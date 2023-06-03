4da857643b35b7027fa78cb4a0b9d59f2844163b
aelyakoubi <115151631+aelyakoubi@users.noreply.github.com>
2023-06-03 13:24:28 +0200

    EventSearch.jsx

--- src/components/EventSearch.jsx
+++ src/components/EventSearch.jsx
@@ -1,5 +1,4 @@
 import { Input } from "@chakra-ui/react";
-
 import React, { useState } from "react";
 
 export const EventSearch = ({ events, setFilteredEvents }) => {
@@ -8,9 +7,14 @@ export const EventSearch = ({ events, setFilteredEvents }) => {
   const handleSearch = (event) => {
     const searchTerm = event.target.value.toLowerCase();
     setSearchTerm(searchTerm);
-    const filteredEvents = events.filter((event) =>
-      event.title.toLowerCase().includes(searchTerm)
-    );
+
+    const filteredEvents = events.filter((event) => {
+      const titleMatch = event.title.toLowerCase().includes(searchTerm);
+      const categoryMatch = event.category.toLowerCase().includes(searchTerm);
+
+      return titleMatch || categoryMatch;
+    });
+
     setFilteredEvents(filteredEvents);
   };
 
